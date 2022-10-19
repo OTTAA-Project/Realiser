f22018bda58d0b46f4404c0e4c32183ac26f15cc
Juanma López <lopezjuanma96@gmail.com>
2022-10-19 15:34:27 -0300

    deleted console logs from last commit

--- functions/getter.js
+++ functions/getter.js
@@ -3,7 +3,6 @@ class dbGetter{
     static alreadyData = {};
 
     static async getOnce(reference, path, defaultTo = undefined){
-        console.log('importing once', path)
         const snapshot = await reference.child(path).get();
         if (snapshot.exists()) return snapshot.val();
         else return defaultTo;
@@ -12,7 +11,6 @@ class dbGetter{
     static async getPersistent(reference, path, defaultTo = undefined){
         if (this.alreadyData[path]) return this.alreadyData[path]
         else {
-            console.log('importing', path, 'for the first time')
             const snapshot = await reference.child(path).get();
             if (snapshot.exists()) {
                 this.alreadyData[path] = snapshot.val();
