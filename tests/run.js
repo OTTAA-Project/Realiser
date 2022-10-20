878c1bf2e83d3d67ca5b04ec47ecf72d6fcfcf7f
Juanma López <lopezjuanma96@gmail.com>
2022-10-20 15:03:00 -0300

    added body to request test on replicate

--- tests/run.js
+++ tests/run.js
@@ -2,11 +2,9 @@ const qnas = require('./qna.js');
 const { createRequestPromise } = require('../functions/requests.js');
 
 for (let i=0; i<qnas.length; i++){
-    console.log(qnas[i].question)
-    continue;
     createRequestPromise({
         host: 'us-central1-ottaaproject-flutter.cloudfunctions.net',
-        path: '/realiser/realise',
+        path: '/realiser/replicate',
         method: 'POST',
         body: qnas[i].question
     }, false)
