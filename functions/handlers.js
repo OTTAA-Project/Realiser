99643d25cd6046e9eeb1214f0b565e9d1e1c24fa
Juanma López <lopezjuanma96@gmail.com>
2022-10-24 11:09:37 -0300

    implementation for issue #11 changed findART to splitART added splitDefinitive added handler from verb token to next token sequence

--- functions/handlers.js
+++ functions/handlers.js
@@ -1,6 +1,28 @@
 const { dbGetter } = require("./getter.js");
 
+async function findART(obj, langRef){
+    const addART = []
+    let i = 0, j = 0;
+    while(i<obj.words.length){
+        if(obj.types[i] === 'NOUN' || obj.types[i] === 'SUBJ'){
+            const articlesGenders = await dbGetter.getPersistent(langRef, 'ARTICLES/GENDERS', {});
+            addART.push([i, articlesGenders[genders[j]]])
+            j++;
+        }
+        i++;
+    }
+    addART.forEach((v, i) => {
+        obj.words.splice(v[0] + i, 0, v[1])
+        obj.types.splice(v[0] + i, 0, 'ART')
+    })
+}
+
 async function handleType(obj, sentence, langRef, src, forces){
+    if(obj.position > 0){
+        if(sentence[obj.position-1].props.next){
+            obj.props.prev = sentence[obj.position-1].props.next;
+        }
+    }
     switch (obj.type){
         case 'VERB':
             obj = await handleVERB(obj, sentence, langRef, src, forces);
@@ -60,15 +82,16 @@ async function handleVERB(obj, sentence, langRef, src, forces){
 }
 
 async function conjugateVERB(obj, langRef, src){ 
-    const defaults = await dbGetter.getPersistent(langRef, 'DEFAULTS', {})
+    const defaults = await dbGetter.getPersistent(langRef, 'DEFAULTS', {});
     const newWordsInf = obj.words.map(word => getVERBInfinitive(word, langRef));
     const newWordsSeq = newWordsInf.map(p => new Promise((resolve) => {
         p.then(inf => dbGetter.getOnce(langRef, `VERBS/SEQUENCES/${inf}`))
         .then(seq => resolve(seq))
-    }))
+    }));
     const newWords = obj.words.map((w, i) => new Promise((resolve) => {
         Promise.all([newWordsInf[i], newWordsSeq[i-1]])
         .then(([inf, seq]) => {
+            seq = i == 0 ? obj.props.prev?.VERB : seq?.VERB
             if(seq){
                 if (seq === 'INF') return inf.split(' ')
                 return Promise.all(inf.split(' ').map(winf => getVERBConjugations(winf, src, langRef, seq)))
@@ -79,8 +102,10 @@ async function conjugateVERB(obj, langRef, src){
                 return Promise.all(inf.split(' ').map(winf => getVERBConjugations(winf, src, langRef, obj.meta.TIME, obj.meta.PERSON)))
             }
         }).then(conj => resolve(conj.join(' ')))
-    }))
-    obj.words = await Promise.all(newWords)
+    }));
+    obj.words = await Promise.all(newWords);
+    obj.infs = await Promise.all(newWordsInf);
+    obj.props.next = await newWordsSeq.at(-1);
     if (obj.words.at(-1).includes(':CON:')){
         obj.words.splice(-1, 1, ...[defaults.CON, obj.words.at(-1).replace(' :CON:', '')])
         obj.types.splice(-1, 0, 'CON')
@@ -127,11 +152,34 @@ async function getVERBInfinitive(word, langRef){
 
 async function handleNOUN(obj, sentence, langRef, src){
     const defaults = await dbGetter.getPersistent(langRef, 'DEFAULTS', {})
+    if(obj.props.prev){
+        if(obj.props.prev[obj.types[0]]){
+            const toPrepend = obj.props.prev[obj.types[0]].EXCEPTION 
+                ? obj.props.prev[obj.types[0]].EXCEPTION[obj.words[0]] || obj.props.prev[obj.types[0]].DEFAULT
+                : obj.props.prev[obj.types[0]].DEFAULT;
+            const toPrependSplit = toPrepend.split(',')
+            for(let t=toPrependSplit.length-1; t<=0; t--){
+                const tp = toPrependSplit[t];
+                switch(tp){
+                    case 'ART':
+                        const articlesGenders = await dbGetter.getPersistent(langRef, 'ARTICLES/GENDERS', {});
+                        obj.words.unshift(articlesGenders[obj.genders[0]])
+                        obj.types.unshift('ART')
+                        break;
+                    case 'NONE':
+                        continue;
+                    default:
+                        obj.words[0] = [tp, obj.words[0]].join(' ')
+                        break;
+                }
+            }
+        }
+    }
     if (obj.composed) {
-        if(obj.types.at(-2) === 'NOUN'){
+        if(obj.types.at(-2) === 'NOUN' || obj.types.at(-2) === 'SUBJ'){
             obj.words.splice(-1, 0, defaults.CON);
             obj.types.splice(-1, 0, "CON");
-        } else if (obj.types.at(-2) === 'ART' && obj.types.at(-3) === 'NOUN'){
+        } else if (obj.types.at(-2) === 'ART' && (obj.types.at(-3) === 'NOUN' || obj.types.at(-3) === 'SUBJ')){
             obj.words.splice(-2, 0, defaults.CON);
             obj.types.splice(-2, 0, "CON");
         }
@@ -149,11 +197,35 @@ async function handleNOUN(obj, sentence, langRef, src){
 
 async function handleSUBJ(obj, sentence, langRef, src){
     const defaults = await dbGetter.getPersistent(langRef, 'DEFAULTS', {})
+    if(obj.props.prev){
+        if(obj.props.prev[obj.types[0]]){
+            const toPrepend = obj.props.prev[obj.types[0]].EXCEPTION 
+                ? obj.props.prev[obj.types[0]].EXCEPTION[obj.words[0]] || obj.props.prev[obj.types[0]].DEFAULT
+                : obj.props.prev[obj.types[0]].DEFAULT;
+            const toPrependSplit = toPrepend.split(',')
+            for(let t=toPrependSplit.length-1; t>=0; t--){
+                const tp = toPrependSplit[t];
+                console.log(tp)
+                switch(tp){
+                    case 'ART':
+                        const articlesGenders = await dbGetter.getPersistent(langRef, 'ARTICLES/GENDERS', {});
+                        obj.words.unshift(articlesGenders[obj.genders[0]])
+                        obj.types.unshift('ART')
+                        break;
+                    case 'NONE':
+                        continue;
+                    default:
+                        obj.words[0] = [tp, obj.words[0]].join(' ')
+                        break;
+                }
+            }
+        }
+    }
     if (obj.composed) {
-        if(obj.types.at(-2) === 'SUBJ'){
+        if(obj.types.at(-2) === 'NOUN' || obj.types.at(-2) === 'SUBJ'){
             obj.words.splice(-1, 0, defaults.CON);
             obj.types.splice(-1, 0, "CON");
-        } else if (obj.types.at(-2) === 'ART' && obj.types.at(-3) === 'SUBJ'){
+        } else if (obj.types.at(-2) === 'ART' && (obj.types.at(-3) === 'NOUN' || obj.types.at(-3) === 'SUBJ')){
             obj.words.splice(-2, 0, defaults.CON);
             obj.types.splice(-2, 0, "CON");
         }
