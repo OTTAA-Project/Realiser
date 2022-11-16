7982051553b326b11082dcf40e83da907cc809c3
Juanma López <lopezjuanma96@gmail.com>
2022-11-16 16:40:56 -0300

    added http tests for #17 using supertest

--- functions/local.js
+++ functions/local.js
@@ -1,10 +1,4 @@
-const functions = require('firebase-functions');
-
 const app = require('./routing.js');
-
-/////////////////
-///// LOCAL /////
-/////////////////
 
 const PORT = 9090;
 app.listen(PORT, () => console.log(`Realiser up locally on http://localhost:${PORT}`))
